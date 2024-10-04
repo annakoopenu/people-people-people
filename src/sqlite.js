5bc1a56e328c08282e8a5dc0ffb27c8cf7b2e229
Glitch (glitch-hello-sqlite) <none>
2024-10-04 18:51:44 +0000

    🙉🚍 Checkpoint ./src/sqlite.js:70542205/20760

--- src/sqlite.js
+++ src/sqlite.js
@@ -1,150 +1,92 @@
-/**
- * Module handles database management
- *
- * Server API calls the methods in here to query and update the SQLite database
- */
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+const csvParser = require('csv-parser');  // <-- Add this line
 
-// Utilities we need
-const fs = require("fs");
+const db = new sqlite3.Database('./.data/sqlite.db');
 
-// Initialize the database
-const dbFile = "./.data/choices.db";
-const exists = fs.existsSync(dbFile);
-const sqlite3 = require("sqlite3").verbose();
-const dbWrapper = require("sqlite");
-let db;
-
-/* 
-We're using the sqlite wrapper so that we can make async / await connections
-- https://www.npmjs.com/package/sqlite
-*/
-dbWrapper
-  .open({
-    filename: dbFile,
-    driver: sqlite3.Database
-  })
-  .then(async dBase => {
-    db = dBase;
-
-    // We use try and catch blocks throughout to handle any database errors
-    try {
-      // The async / await syntax lets us write the db operations in a way that won't block the app
-      if (!exists) {
-        // Database doesn't exist yet - create Choices and Log tables
-        await db.run(
-          "CREATE TABLE Choices (id INTEGER PRIMARY KEY AUTOINCREMENT, language TEXT, picks INTEGER)"
-        );
-
-        // Add default choices to table
-        await db.run(
-          "INSERT INTO Choices (language, picks) VALUES ('HTML', 0), ('JavaScript', 0), ('CSS', 0)"
-        );
-
-        // Log can start empty - we'll insert a new record whenever the user chooses a poll option
-        await db.run(
-          "CREATE TABLE Log (id INTEGER PRIMARY KEY AUTOINCREMENT, choice TEXT, time STRING)"
-        );
-      } else {
-        // We have a database already - write Choices records to log for info
-        console.log(await db.all("SELECT * from Choices"));
-
-        //If you need to remove a table from the database use this syntax
-        //db.run("DROP TABLE Logs"); //will fail if the table doesn't exist
-      }
-    } catch (dbError) {
-      console.error(dbError);
-    }
-  });
-
-// Our server script will call these methods to connect to the db
-module.exports = {
+// Create tables
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS people (
+    id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    bio TEXT, 
+    year_of_birth INTEGER, 
+    date_of_birth TEXT, 
+    wiki_link TEXT, 
+    more TEXT
+  )`);
   
-  /**
-   * Get the options in the database
-   *
-   * Return everything in the Choices table
-   * Throw an error in case of db connection issues
-   */
-  getOptions: async () => {
-    // We use a try catch block in case of db errors
-    try {
-      return await db.all("SELECT * from Choices");
-    } catch (dbError) {
-      // Database connection error
-      console.error(dbError);
-    }
-  },
+  db.run(`CREATE TABLE IF NOT EXISTS people_quotes (
+    person_id INTEGER, 
+    title TEXT, 
+    context TEXT, 
+    link TEXT, 
+    more TEXT
+  )`);
+  
+  db.run(`CREATE TABLE IF NOT EXISTS people_creations (
+    person_id INTEGER, 
+    title TEXT, 
+    type TEXT, 
+    link TEXT, 
+    more TEXT
+  )`);
+  
+  db.run(`CREATE TABLE IF NOT EXISTS people_connections (
+    person1_id INTEGER, 
+    person1_name TEXT, 
+    person2_id INTEGER, 
+    person2_name TEXT, 
+    connection TEXT, 
+    more TEXT
+  )`);
+});
 
-  /**
-   * Process a user vote
-   *
-   * Receive the user vote string from server
-   * Add a log entry
-   * Find and update the chosen option
-   * Return the updated list of votes
-   */
-  processVote: async vote => {
-    // Insert new Log table entry indicating the user choice and timestamp
-    try {
-      // Check the vote is valid
-      const option = await db.all(
-        "SELECT * from Choices WHERE language = ?",
-        vote
-      );
-      if (option.length > 0) {
-        // Build the user data from the front-end and the current time into the sql query
-        await db.run("INSERT INTO Log (choice, time) VALUES (?, ?)", [
-          vote,
-          new Date().toISOString()
-        ]);
+// Function to import CSV data into SQLite
+function importCSVData() {
+  // Import 'people.csv'
+  fs.createReadStream('./data/people.csv')
+    .pipe(csvParser())
+    .on('data', (row) => {
+      db.run('INSERT INTO people (id, name, bio, year_of_birth, date_of_birth, wiki_link, more) VALUES (?, ?, ?, ?, ?, ?, ?)', 
+        [row.id, row.name, row.bio, row.year_of_birth, row.date_of_birth, row.wiki_link, row.more]);
+    })
+    .on('end', () => {
+      console.log('people.csv successfully imported');
+    });
 
-        // Update the number of times the choice has been picked by adding one to it
-        await db.run(
-          "UPDATE Choices SET picks = picks + 1 WHERE language = ?",
-          vote
-        );
-      }
+  // Import 'people_quotes.csv'
+  fs.createReadStream('./data/people_quotes.csv')
+    .pipe(csvParser())
+    .on('data', (row) => {
+      db.run('INSERT INTO people_quotes (person_id, title, context, link, more) VALUES (?, ?, ?, ?, ?)', 
+        [row.person_id, row.title, row.context, row.link, row.more]);
+    })
+    .on('end', () => {
+      console.log('people_quotes.csv successfully imported');
+    });
 
-      // Return the choices so far - page will build these into a chart
-      return await db.all("SELECT * from Choices");
-    } catch (dbError) {
-      console.error(dbError);
-    }
-  },
+  // Import 'people_creations.csv'
+  fs.createReadStream('./data/people_creations.csv')
+    .pipe(csvParser())
+    .on('data', (row) => {
+      db.run('INSERT INTO people_creations (person_id, title, type, link, more) VALUES (?, ?, ?, ?, ?)', 
+        [row.person_id, row.title, row.type, row.link, row.more]);
+    })
+    .on('end', () => {
+      console.log('people_creations.csv successfully imported');
+    });
 
-  /**
-   * Get logs
-   *
-   * Return choice and time fields from all records in the Log table
-   */
-  getLogs: async () => {
-    // Return most recent 20
-    try {
-      // Return the array of log entries to admin page
-      return await db.all("SELECT * from Log ORDER BY time DESC LIMIT 20");
-    } catch (dbError) {
-      console.error(dbError);
-    }
-  },
+  // Import 'people_connections.csv'
+  fs.createReadStream('./data/people_connections.csv')
+    .pipe(csvParser())
+    .on('data', (row) => {
+      db.run('INSERT INTO people_connections (person1_id, person1_name, person2_id, person2_name, connection, more) VALUES (?, ?, ?, ?, ?, ?)', 
+        [row.person1_id, row.person1_name, row.person2_id, row.person2_name, row.connection, row.more]);
+    })
+    .on('end', () => {
+      console.log('people_connections.csv successfully imported');
+    });
+}
 
-  /**
-   * Clear logs and reset votes
-   *
-   * Destroy everything in Log table
-   * Reset votes in Choices table to zero
-   */
-  clearHistory: async () => {
-    try {
-      // Delete the logs
-      await db.run("DELETE from Log");
-
-      // Reset the vote numbers
-      await db.run("UPDATE Choices SET picks = 0");
-
-      // Return empty array
-      return [];
-    } catch (dbError) {
-      console.error(dbError);
-    }
-  }
-};
+importCSVData();
